c42b8fe660893bc341bf8cbccc8f83974df7b723
Juan Luis <juanlupriv24@gmail.com>
2025-02-20 04:07:04 +0100

    Card Import

---
+++ src/components/Card.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+    </script>
+
+<Card>
+    <CardHeader>
+      <CardTitle>Card Title</CardTitle>
+      <CardDescription>Card Description</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p>Card Content</p>
+    </CardContent>
+    <CardFooter>
+      <p>Card Footer</p>
+    </CardFooter>
+  </Card>
