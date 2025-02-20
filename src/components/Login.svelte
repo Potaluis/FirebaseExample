f64a891f2012f37a9c85d6b64e81824faf0b167a
Juan Luis <juanlupriv24@gmail.com>
2025-02-20 02:43:30 +0100

    Better Typing + TailWind

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -17,12 +17,32 @@
   </script>
   
   <main class="min-h-screen flex items-center justify-center bg-[#1a1a1a]">
+  <div class="w-full max-w-md space-y-6">
+
     <h1 class="text-8xl font-bold text-center text-white mb-8">LOG IN</h1>
-    <input type="email" bind:value={email} placeholder="Correo electrónico" class="w-full px-4 py-3 border border-[#DDDDDD] rounded-xl placeholder-gray-400 focus:ring-2 focus:ring-blue-600 focus:border-blue-600 outline-none transition-all" />
-    <input type="password" bind:value={password} placeholder="Contraseña" class="w-full px-4 py-3 border border-[#DDDDDD] rounded-xl placeholder-gray-400 focus:ring-2 focus:ring-blue-600 focus:border-blue-600 outline-none transition-all"/>
-    <button on:click={login} class="w-full px-8 py-3 text-lg font-bold text-white bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200">      >Ingresar</button>
+
+    <div class="space-y-4">
+    <input 
+    type="email" 
+    bind:value={email} 
+    placeholder="Correo electrónico" 
+    class="w-full px-4 py-3 border border-[#DDDDDD] rounded-xl placeholder-gray-400 focus:ring-2 focus:ring-blue-600 focus:border-blue-600 outline-none transition-all" />
+
+    <input type="password" 
+    bind:value={password} 
+    placeholder="Contraseña" 
+    class="w-full px-4 py-3 border border-[#DDDDDD] rounded-xl placeholder-gray-400 focus:ring-2 focus:ring-blue-600 focus:border-blue-600 outline-none transition-all"/>
+
+    <button 
+    on:click={login} 
+    class="w-full px-8 py-3 text-lg font-bold text-white bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200">
+    Ingresar</button>
+
     {#if errorMessage}
       <p style="color: red;">{errorMessage}</p>
     {/if}
+    
+    </div>
+    </div>
   </main>
   
